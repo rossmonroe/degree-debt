d659c44a861b71882b9cbf9b95f7f6ba74c9cc6d
Daiana Kaplan <daianak@uw.edu>
2019-02-27 16:18:49 -0800

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ class About extends Component {
         <div className='separator margin-10'></div>
         <div class='page-content'>
           <h3>About</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor consequat id porta nibh venenatis. Rhoncus mattis rhoncus urna neque viverra justo. Et netus et malesuada fames ac turpis egestas. Eu non diam phasellus vestibulum. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla.</p>
+          <p>Editing paragraph one...., sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor consequat id porta nibh venenatis. Rhoncus mattis rhoncus urna neque viverra justo. Et netus et malesuada fames ac turpis egestas. Eu non diam phasellus vestibulum. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla.</p>
           <p>Urna duis convallis convallis tellus. Venenatis lectus magna fringilla urna porttitor rhoncus. Enim neque volutpat ac tincidunt vitae semper quis lectus. Ultrices tincidunt arcu non sodales neque.</p>
           <p>Sed egestas egestas fringilla phasellus. Elementum nibh tellus molestie nunc non. Suscipit tellus mauris a diam. Nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet. Adipiscing bibendum est ultricies integer quis auctor. Ut venenatis tellus in metus vulputate. Arcu dictum varius duis at consectetur lorem donec. Lorem mollis aliquam ut porttitor leo a diam. Mi proin sed libero enim sed faucibus turpis in eu. Faucibus nisl tincidunt eget nullam non nisi. Fermentum posuere urna nec tincidunt praesent semper feugiat nibh.</p>
         </div>
