f3d7e1175210a1eac12d2664df8693e210df17c2
Daiana Kaplan <daianak@uw.edu>
2019-03-04 16:35:36 -0800

    Form sizes

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -6,7 +6,7 @@ class Learn extends Component {
   render() {
     return (
       <div class='page-wrapper'>
-      <div className='separator margin-10'></div>
+        <div className='separator margin-10'></div>
         <div class='page-content'>
           <h3>Learn</h3>
         </div>
--- src/pages/StudentDebtCalculator.js
+++ src/pages/StudentDebtCalculator.js
@@ -9,47 +9,47 @@ class StudentDebtCalculator extends Component {
   render() {
     return (
       <div className='page-wrapper'>
-        <HeroImage/>
+        <HeroImage />
         <div className='separator'></div>
         <div className='page-content'>
           <div id="debt-calculator">
-          <h3>Student Debt Calculator:</h3>
-          <div className='opacity-50 separator margin-10'></div>
+            <h3>Student Debt Calculator:</h3>
+            <div className='opacity-50 separator margin-10'></div>
             <form method="get" action="#" className="form-input clear">
-                <p>Degree Type:</p>
-                <input name="q" size="40" type="text" list="degree-types" placeholder="Graduate Degree"/>
-                <datalist id="degree-types">
-                    <option value="Bachelors Degree"/> 
-                    <option value="Red"/> 
-                    <option value="Blue"/> 
-                    <option value="Yellow"/> 
-                    <option value="Orange"/> 
-                    <option value="Purple"/> 
-                    <option value="Black"/> 
-                    <option value="White"/> 
-                    <option value="Gray"/> 
-                    <option value="Plaid"/> 
-                </datalist>
+              <p>Degree Type:</p>
+              <input name="q" size="40" type="text" list="degree-types" placeholder="Graduate Degree" />
+              <datalist id="degree-types">
+                <option value="Bachelors Degree" />
+                <option value="Associates Degree" />
+                <option value="Masters Degree" />
+                <option value="Yellow" />
+                <option value="Orange" />
+                <option value="Purple" />
+                <option value="Black" />
+                <option value="White" />
+                <option value="Gray" />
+                <option value="Plaid" />
+              </datalist>
 
-                <p>Major:</p>
-                <input name="q" size="40" type="text" placeholder="Electrical Engineering"/>
-                <p>Tuiton Type:</p>
-                <ul className="radio-buttons">
-                  <li>
-                    <input type="radio" id="I-option" name="selector"/>
-                    <label for="I-option">In State</label>
-                    <div class="check"><div class="inside"></div></div>
-                  </li>
-                  <li>
-                    <input type="radio" id="O-option" name="selector"/>
-                    <label for="O-option">Out of State</label>
-                    <div class="check"><div class="inside"></div></div>
-                  </li>
-                </ul>
-                <p className='clear'>Tuition Paid: <sub>(Scholarships, Cash, Grants.. etc.)</sub></p>
-                <input name="q" size="40" type="text" placeholder="$8,000"/>
-                <input type="submit" className='button' value="Calculate" />
-                <div className='clear'></div>
+              <p>Major:</p>
+              <input name="q" size="40" type="text" placeholder="Electrical Engineering" />
+              <p>Tuiton Type:</p>
+              <ul className="radio-buttons">
+                <li>
+                  <input type="radio" id="I-option" name="selector" />
+                  <label for="I-option">In State</label>
+                  <div class="check"><div class="inside"></div></div>
+                </li>
+                <li>
+                  <input type="radio" id="O-option" name="selector" />
+                  <label for="O-option">Out of State</label>
+                  <div class="check"><div class="inside"></div></div>
+                </li>
+              </ul>
+              <p className='clear'>Tuition Paid: <sub>(Scholarships, Cash, Grants.. etc.)</sub></p>
+              <input name="q" size="40" type="text" placeholder="$8,000" />
+              <input type="submit" className='button' value="Calculate" />
+              <div className='clear'></div>
             </form>
           </div>
         </div>
