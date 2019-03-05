fb70894e12e6dcdcb27ad45fc37f4e8e0c2300fe
Daiana Kaplan <daianak@uw.edu>
2019-03-04 17:37:50 -0800

    Add info to learn page

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 import '../style/css/Global.css';
 
+import AboutInfo from '../components/AboutInfo.js';
+
 class Learn extends Component {
   render() {
     return (
       <div class='page-wrapper'>
+        <AboutInfo />
         <div className='separator margin-10'></div>
         <div class='page-content'>
-          <h3>Learn</h3>
         </div>
       </div>
     );
