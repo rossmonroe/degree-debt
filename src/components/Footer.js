cd59ed61b609d661ec44b6e47e95d356fc98dcaa
Daiana Kaplan <daianak@uw.edu>
2019-03-04 17:22:26 -0800

    Took out compare degrees from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,17 +8,16 @@ class Footer extends Component {
   render() {
     return (
       <footer id='footer'>
-        
+
         <div id="footer-content">
           <div className='opacity-50 separator margin-10'></div>
           <div id='footer-logo'></div>
           <div className='footer-navigation'>
-              <ul>
-                <li><Link className='nav-link' to= '/'>Student Debt Calculator</Link></li>
-                <li><Link className='nav-link' to="/compare">Compare Degrees</Link></li>
-                <li><Link className='nav-link' to="/learn">Learn</Link></li>
-                <li><Link className='nav-link' to="/about">About</Link></li>
-              </ul>
+            <ul>
+              <li><Link className='nav-link' to='/'>Student Debt Calculator</Link></li>
+              <li><Link className='nav-link' to="/learn">Learn</Link></li>
+              <li><Link className='nav-link' to="/about">About</Link></li>
+            </ul>
           </div>
           <div className='clear'></div>
         </div>
