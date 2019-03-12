0fa835e507ee8f25f0efc3610bd251ba450e7cf2
Daiana Kaplan <daianak@uw.edu>
2019-03-12 16:37:41 -0700

    Updating Graphs

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -6,9 +6,24 @@ import '../../node_modules/react-vis/dist/style.css';
 import { XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis } from 'react-vis';
 
 class Results extends Component {
+
   render() {
 
-    const data = [
+    const data1 = [
+      { x: 0, y: 8 },
+      { x: 1, y: 5 },
+      { x: 2, y: 4 },
+      { x: 3, y: 9 },
+      { x: 4, y: 1 },
+      { x: 5, y: 7 },
+      { x: 6, y: 6 },
+      { x: 7, y: 3 },
+      { x: 8, y: 2 },
+      { x: 9, y: 0 }
+    ];
+
+
+    const data2 = [
       { x: 0, y: 8 },
       { x: 1, y: 5 },
       { x: 2, y: 4 },
@@ -23,19 +38,35 @@ class Results extends Component {
 
 
 
+
     return (
       <div class='page-wrapper' >
         <div className='separator margin-10'></div>
         <div class='page-content'>
           <h3>Your Results!</h3>
 
-          <XYPlot height={300} width={600}>
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <XAxis />
-            <YAxis />
-            <LineSeries data={data} />
-          </XYPlot>
+          <div class='chart'>
+            <h4> Potential Salary Over Time </h4>
+            <XYPlot height={300} width={600}>
+              <VerticalGridLines />
+              <HorizontalGridLines />
+              <XAxis title="Years" />
+              <YAxis title="Money (USD)" />
+              <LineSeries data={data1} />
+            </XYPlot>
+
+          </div>
+
+          <div class='chart'>
+            <h4> Projected Payment Over Time</h4>
+            <XYPlot height={300} width={600}>
+              <VerticalGridLines />
+              <HorizontalGridLines />
+              <XAxis />
+              <YAxis />
+              <LineSeries data={data2} />
+            </XYPlot>
+          </div>
         </div>
       </div >
     );
