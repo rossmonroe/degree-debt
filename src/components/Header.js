43e9035dd93c18856c63bb2566e503d271ee91f9
Ross Monroe <rossmonroe@me.com>
2019-03-12 16:33:02 -0700

    updated anchors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
+import ScrollableAnchor from 'react-scrollable-anchor'
 
 import '../style/css/Global.css';
 
@@ -11,7 +12,7 @@ class Header extends Component {
       <div className='header'>
       <div className="header-content">
         <Navbar expand="md">
-        <Navbar.Brand href="#home"><div id='top-logo'></div></Navbar.Brand>
+        <Navbar.Brand href="/"><div id='top-logo'></div></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
           <Nav>
@@ -24,7 +25,7 @@ class Header extends Component {
       </div>
       <div id="header-title">
         <h1>Learn how your degree choice <br/>affects your financial future.</h1>
-        <a href="#calculator"><div className='button header-button'>Get Started!</div></a>
+        <Link className='nav-link' to="/#calculator" href="#calculator"><div className='button header-button'>Get Started!</div></Link>
       </div>
       </div>
     );
